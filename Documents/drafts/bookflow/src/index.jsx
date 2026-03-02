ad7d927252e1bf6dd398c98625f1e7ba240d8abc
papchenko <nikopapchenko@gmail.com>
2026-03-02 19:46:14 +0200

    fix spa routing

--- Documents/drafts/bookflow/src/index.jsx
+++ Documents/drafts/bookflow/src/index.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+const params = new URLSearchParams(window.location.search);
+const redirect = params.get("redirect");
+
+if (redirect) {
+  window.history.replaceState(null, null, redirect);
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
